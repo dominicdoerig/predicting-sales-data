ad14b3c00052e3e0035d3937a2f88f758225998c
Dominic Doerig <dominic.dorig@gmail.com>
2020-03-28 12:05:52 +0100

    simplify reduce_mem_usage method

--- m5_challenge/utils.py
+++ m5_challenge/utils.py
@@ -76,35 +76,18 @@ def reduce_mem_usage(df, verbose=True):
     Reduces the required memory of a pandas.Dataframe by reducing the storage capacity of numerical data types
 
     """
-    numerics = ["int16", "int32", "int64", "float16", "float32", "float64"]
     start_mem = df.memory_usage().sum() / 1024 ** 2
-    for col in df.columns:
-        col_type = df[col].dtypes
-        if col_type in numerics:
-            c_min = df[col].min()
-            c_max = df[col].max()
-            if str(col_type)[:3] == "int":
-                if c_min > np.iinfo(np.int8).min and c_max < np.iinfo(np.int8).max:
-                    df[col] = df[col].astype(np.int8)
-                elif c_min > np.iinfo(np.int16).min and c_max < np.iinfo(np.int16).max:
-                    df[col] = df[col].astype(np.int16)
-                elif c_min > np.iinfo(np.int32).min and c_max < np.iinfo(np.int32).max:
-                    df[col] = df[col].astype(np.int32)
-                elif c_min > np.iinfo(np.int64).min and c_max < np.iinfo(np.int64).max:
-                    df[col] = df[col].astype(np.int64)
-            else:
-                if (
-                        c_min > np.finfo(np.float16).min
-                        and c_max < np.finfo(np.float16).max
-                ):
-                    df[col] = df[col].astype(np.float16)
-                elif (
-                        c_min > np.finfo(np.float32).min
-                        and c_max < np.finfo(np.float32).max
-                ):
-                    df[col] = df[col].astype(np.float32)
-                else:
-                    df[col] = df[col].astype(np.float64)
+
+    #getting columns names with int and float dtypes
+    float_cols = df.select_dtypes(include=['float']).columns
+    int_cols = df.select_dtypes(include=['integer']).columns
+
+    #donwcasting the values
+    for col in float_cols:
+        df[col] = pd.to_numeric(df[col],downcast='float')
+    for col in int_cols:
+        df[col] = pd.to_numeric(df[col],downcast='integer')
+
     end_mem = df.memory_usage().sum() / 1024 ** 2
     if verbose:
         print(
